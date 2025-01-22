2725b3fe3a10db1f713fff1b5db82867d2c6d024
Fredrik Skärberg <73640157+SkariZ@users.noreply.github.com>
2025-01-22 11:02:01 +0100

    Update application.py

--- tomodpdt/application.py
+++ tomodpdt/application.py
@@ -2,8 +2,9 @@ import deeplay as dl
 import deeptrack as dt
 import torch
 import torch.nn as nn
-import numpy as np
-import matplotlib.pyplot as plt
+import torch.nn.functional as F
+
+# Importing the necessary modules
 import estimate_rotations_from_latent as erfl
 
 class Tomography(dl.Application):
@@ -15,30 +16,44 @@ class Tomography(dl.Application):
                  optimizer=None,
                  volume_init=None,
                  **kwargs,
-
-
-
                  ):
+        
         self.N=volume_size[0]
-        self.vae_model=vae_model or dl.VariationalAutoEncoder(input_size=(self.volume_size[0],self.volume_size[1]))
-        self.encoder=self.vae_model.encoder
-        self.fc_mu=self.vae_model.fc_mu
-        self.imaging_model=imaging_model or Projections
-        self.device=self.vae_model.device
-        self.initial_volume=initial_volume
+        self.vae_model = vae_model or dl.VariationalAutoEncoder(input_size=(self.volume_size[0],self.volume_size[1]))
+        self.encoder = self.vae_model.encoder
+        self.fc_mu = self.vae_model.fc_mu
+        self.imaging_model = imaging_model if imaging_model is not None else "projection_model"
+        self.device = self.vae_model.device
+        self.initial_volume = initial_volume
         self.volume_init = volume_init
         super().__init__(**kwargs)
 
-        self.optimizer = optimizer or Adam(lr=5e-3)
-    def initialize_parameters(self,projections,**kwargs):
+        # Set the optimizer
+        self.optimizer = optimizer or torch.nn.optimizers.Adam(lr=5e-3)
+
+        # Set the imaging model
+        if imaging_model=="projection_model":
+            def projection(volume):
+                return torch.sum(volume, dim=0)
+            self.imaging_model = projection
+
+        # Set the grid for rotating the volume
+        x = torch.arange(self.N) - self.N / 2
+        self.xx, self.yy, self.zz = torch.meshgrid(x, x, x, indexing='ij')
+        self.grid = torch.stack([self.xx, self.yy, self.zz], dim=-1).to(self.device)
+
+                
+    def initialize_parameters(self, projections,**kwargs):
         latent_space = self.vae.fc_mu(self.vae.encoder(projections))
         self.latent = latent_space
         self.volume = self.initialize_volume()
+
+        # Retrieve the initial rotation parameters
         self.rotation_params = erfl(latent_space,**kwargs) #Later: add axis also
         
-        # Setting frames to the number of quaternions
+        # Setting frames to the number of rotations
         self.frames = projections[:len(self.quaternions)]
-        
+
         @self.optimizer.params
         def params(self):
             return self.parameters()
@@ -78,13 +93,20 @@ class Tomography(dl.Application):
             self.volume = nn.Parameter(torch.zeros(self.N, self.N, self.N, device=self.device))
     
     def forward(self,idx):
-        volume=self.volume
-        rotations=self.get_quaternions(self.rotation_params)[idx]
+        """
+        Forward pass of the model.
+        """
+        volume = self.volume
+        rotations = self.get_quaternions(self.rotation_params)[idx]
+
         batch_size = rotations.shape[0]
         estimated_projections = torch.zeros(batch_size, self.N, self.N, device=self.device)
+
+        # Rotate the volume and estimate the projections
         for i in range(batch_size):
-            rotated_volume=self.apply_rotation(volume,rotations[i])
-            estimated_projections[i]=self.imaging_model(rotated_volume)
+            rotated_volume = self.apply_rotation(volume, rotations[i])
+            estimated_projections[i] = self.imaging_model(rotated_volume)
+
         return estimated_projections
     
     def training_step(self,batch,batch_idx):
@@ -92,12 +114,13 @@ class Tomography(dl.Application):
 
         
         
-        yhat=self.forward(batch_idx) #Estimated projections
+        yhat = self.forward(batch_idx) #Estimated projections
 
         latent_space = self.fc_mu(self.encoder(yhat))
 
-        proj_loss,latent_loss=self.compute_loss(yhat,latent_space,batch,batch_idx)
+        proj_loss, latent_loss = self.compute_loss(yhat, latent_space, batch, batch_idx)
         tot_loss=proj_loss+latent_loss
+
         loss = {"proj_loss": proj_loss, "latent_loss": latent_loss, "total_loss": tot_loss}
         for name, v in loss.items():
             self.log(
@@ -109,6 +132,7 @@ class Tomography(dl.Application):
                 logger=True,
             )
         return tot_loss
+    
     def compute_loss(yhat,y):
         XXX
         return XXX
